f1e4519e9dc9be078c7232aafa70218b038999c1
RostyslavKov <rostyslav.kovalenko@gmail.com>
2024-01-01 20:58:25 +0200

    Add ENV

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
-
+import dotenv from 'dotenv';
+dotenv.config();
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -20,13 +21,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', {open:'never'}]],
+  reporter: [
+    ['html', {open:'never'}],
+    ['line'], ['blob']
+  ],
   globalTimeout: 1800000,
   globalSetup: 'globalSetup.ts',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://demoqa.com/',
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
